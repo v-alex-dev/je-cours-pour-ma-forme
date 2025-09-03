202ce81674c69abdf96ef82962ccf57d0a713f2d
dev_vens <vensalex1991@gmail.com>
2025-09-03 10:27:49 +0200

    Add CORS support for API routes in Nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,11 @@ export default defineNuxtConfig({
       SUPABASE_ANON_KEY: process.env.SUPABASE_PUBLISHABLE_KEY,
     }
   },
+  nitro:{
+    routeRules:{
+      '/api/**': { cors: true }
+    }
+  },
   modules: ['@nuxt/icon', '@nuxt/fonts', '@nuxt/image', '@pinia/nuxt'],
   css: ['~/assets/css/main.css'],
   vite: {
